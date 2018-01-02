afd103b06622830d023266a304bde3cfdf549e95
Rob Maclean <rjmac@live.co.uk>
2018-01-02 10:31:15 +0000

    refactored functional test

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_rows_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new onlie to-do app.
         # She goes to check out its hompage.
@@ -37,21 +42,23 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_rows_in_list_table('1: Buy peacock feathers')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        # self.assertTrue(
-        #     any(row.text == '1: Buy peacock feathers' for row in rows),
-        #     f"New to-do item did not appear in table. Contents were:\n{table.text}"
-        # )
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # There is still a text box inviting her to add another item,
         # She enters "Use peacock fathers to make a fly"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        self.fail('Finish the test!')
+
 
         # The page updates again, and now shows both items on her lists
+        self.check_for_rows_in_list_table('1: Buy peacock feathers')
+        self.check_for_rows_in_list_table('2: Use peacock feathers to make a fly')
+
+        self.fail('Finish the test!')
 
 
         # Edth wonders wheather the site will remember her list, Then She
