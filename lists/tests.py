87b229ff80a6a17212c33d8f662778f44219feff
Rob Maclean <rjmac@live.co.uk>
2018-01-02 14:14:49 +0000

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -6,29 +6,12 @@ from django.template.loader import render_to_string
 from lists.views import home_page
 from lists.models import Item
 
-# class SmokeTest(TestCase):
-#
-#     def test_bad_maths(self):
-#         self.assertEqual(1 + 1, 3)
+
 
 class HomePageTest(TestCase):
 
-    # def test_root_url_resolves_to_home_page_view(self):
-    #     found = resolve('/')
-    #     self.assertEqual(found.func, home_page)
 
 
-    def test_home_page_returns_correct_html(self):
-        # request = HttpRequest()
-        # response = home_page(request)
-        response = self.client.get('/')
-        # html = response.content.decode('utf8')
-        # # expected_html = render_to_string('home.html')
-        # # self.assertEqual(html, expected_html)
-        # self.assertTrue(html.startswith('<html>'))
-        # self.assertIn('<title>To-Do lists</title>', html)
-        # self.assertTrue(html.strip().endswith('</html>'))
-        self.assertTemplateUsed(response, 'home.html')
 
     def test_uses_home_template(self):
         response = self.client.get('/')
@@ -36,8 +19,30 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+        # self.assertIn('A new list item', response.content.decode())
+        # self.assertTemplateUsed(response, 'home.html')
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
 
 class ItemModelTest(TestCase):
 
